8b11f0d9325b160b4fa86ce84162ebb314b328a9
Matthias <matthias-warken@t-online.de>
2026-08-13 11:58:24 +0200

    vektor und punke hinzugefügt von zwei winkeln!

--- block_sphere/visualizer.py
+++ block_sphere/visualizer.py
@@ -13,9 +13,22 @@ x = np.outer(np.cos(u), np.sin(v))
 y = np.outer(np.sin(u), np.sin(v))
 z = np.outer(np.ones_like(u), np.cos(v))
 
-ax.plot_wireframe(x, y, z, color='0', alpha=1, linewidth=0.2)
-ax.set_box_aspect([1,1,1])
 
+def coords_from_angle(theta, phi):
+    px = np.sin(theta) * np.cos(phi)
+    py = np.sin(theta) * np.sin(phi)
+    pz = np.cos(theta)
+    return px, py, pz
+
+test_theta = np.pi / 2
+test_phi = np.pi * 3/4
+
+test_coords = coords_from_angle(test_theta, test_phi)
+ax.quiver(0, 0, 0, test_coords[0], test_coords[1], test_coords[2])
+
+#alle axen einstellungen
+ax.plot_wireframe(x, y, z, color='0', alpha=0.3, linewidth=0.2)
+ax.set_box_aspect([1,1,1])
 
 ax.grid(False)
 
@@ -36,9 +49,11 @@ ax.plot([0, 0], [0, 0], [-lim, lim], color='k', lw=1)
 
 ax.set_axis_off() 
 
-ax.text(0, 0, lim, "|0⟩", fontsize = 16)
-ax.text(0, 0, -lim, "|1⟩", fontsize = 16)
-ax.text(0, lim, 0, "|+⟩", fontsize = 16)
-ax.text(0, -lim, 0, "|-⟩", fontsize = 16)
+ax.text(0, 0, 1, "|0⟩", fontsize = 16)
+ax.text(0, 0, -1, "|1⟩", fontsize = 16)
+ax.text(0, 1, 0, "|+⟩", fontsize = 16)
+ax.text(0, -1, 0, "|-⟩", fontsize = 16)
+ax.text(1, 0, 0, "|i⟩", fontsize = 16)
+ax.text(-1, 0, 0, "|-i⟩", fontsize = 16)
 
 plt.show()
