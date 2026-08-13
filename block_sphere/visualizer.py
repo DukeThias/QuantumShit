4d73d65ee81081d2ce39868f7004b9a430123a71
Matthias <matthias-warken@t-online.de>
2026-08-13 11:08:39 +0200

    schönere kugel

--- block_sphere/visualizer.py
+++ block_sphere/visualizer.py
@@ -7,12 +7,15 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 
-u = np.linspace(0, 2 * np.pi, 10)
-v = np.linspace(0, np.pi, 10)
+u = np.linspace(0, 2 * np.pi, 50)
+v = np.linspace(0, np.pi, 50)
 x = np.outer(np.cos(u), np.sin(v))
 y = np.outer(np.sin(u), np.sin(v))
 z = np.outer(np.ones_like(u), np.cos(v))
 
-ax.plot_wireframe(x, y, z, color='0', alpha=1)
+print(x)
+print(y)
+print(z)
+ax.plot_wireframe(x, y, z, color='0', alpha=1, linewidth=0.2)
 ax.set_box_aspect([1,1,1])
 plt.show()
